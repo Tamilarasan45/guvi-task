e10904e8ec941d2de86961e1d8115b704812b2aa
Tamilarasan45 <60417070+Tamilarasan45@users.noreply.github.com>
2020-01-29 12:14:24 +0530

    Update pascal triangle.py

--- pascal triangle.py
+++ pascal triangle.py
@@ -1,4 +1,9 @@
-for i in range(0, 5):
-    for j in range(0, i+1):
-        print("*", end="")
-    print()
+n=5
+a=2*n-2
+for i in range(0,n):
+    for j in range(0,a):
+        print(end=" ")
+    a=a-1
+    for k in range(0,i+1):
+        print("*",end=" ")
+    print("\r")
