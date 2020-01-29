1ad13a7e7149e745affdb82863ae7c103747cba6
Tamilarasan45 <60417070+Tamilarasan45@users.noreply.github.com>
2020-01-29 10:43:49 +0530

    Update number guessing game.py

--- number guessing game.py
+++ number guessing game.py
@@ -1,1 +1,14 @@
+import random
+chance=5
+a =random.randint(1,15)
+print("Guess a number (between 1 and 15):")  
+while chance!=0: 
+    b = int(input())
+    if a==b:
+        print("Congratulation YOU WON!!!") 
+        break
+    else:
+        print("try again")
+    chance=chance-1
+print("game over")
 
